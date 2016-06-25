dbe64878168e17aead9f186b8a3e357c604f2e23
矢野史洋 <a2.mj.pop@gmail.com>
2016-06-26 02:09:39 +0900

    add チェック後のアニメーションを変化させた

--- SampleQuestionnaire/ModalViewController.swift
+++ SampleQuestionnaire/ModalViewController.swift
@@ -42,6 +42,25 @@ class ModalViewController: UIViewController {
             UIImageView.animateWithDuration(0.5) { () -> Void in
                 self.checkingImageView.alpha = 1.0
             }
+        
+        // アニメーションの時間を0.5秒に設定.
+        UIImageView.animateWithDuration(0.5,
+            animations: { () -> Void in
+                // 縮小用アフィン行列を作成.
+                self.checkingImageView.transform = CGAffineTransformMakeScale(1.5, 1.5)
+            }) // 連続したアニメーション処理.
+            { (Bool) -> Void in
+                UIImageView.animateWithDuration(0.5,
+                    // アニメーション中の処理.
+                    animations: { () -> Void in
+                        // 拡大用アフィン行列を作成.
+                        self.checkingImageView.transform = CGAffineTransformMakeScale(1, 1)
+                    }) // アニメーション完了時の処理.
+                    { (Bool) -> Void in
+                        // 大きさを元に戻す.
+                        self.checkingImageView.transform = CGAffineTransformMakeScale(1, 1)
+                }
+        }
     }
     
     override func didReceiveMemoryWarning() {
